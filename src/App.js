5c2268f669391a804ea26c952328b2992bb36620
Julia Schwerdtfeger <js.schwerdt@gmail.com>
2023-12-01 12:13:07 +0100

    challenge 2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import React from "react";
 import "./styles.css";
 
+function HelloWorldArticle() {
+  return (
+    <article>
+      <h1>Article Heading</h1>
+      <p>Article Text</p>
+    </article>
+  );
+}
+
 export default function App() {
-  return <div>Replace me with your Component!</div>;
+  return HelloWorldArticle();
 }
